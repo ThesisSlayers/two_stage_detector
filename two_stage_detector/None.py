22541ce1088cb03933ea5a589def4a186f7ad6ca
Morpheus <addfor@morheus.com>
2021-08-06 11:17:22 +0200

    created BBox class and refactored inference and create_two_detector_results

---
+++ two_stage_detector/None.py
@@ -0,0 +1,361 @@
+
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
+
+# Cell
+import tensorflow as tf
+import logging
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import json
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+
+from utils import *
+
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+
+%config Completer.use_jedi = False
